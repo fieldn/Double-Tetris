d4a28f72bee02e65c1762f73ede1bbaf8ac89601
Noah Field <fieldn@purdue.edu>
2016-04-21 23:00:18 -0400

    Removed bootstrap from requirement

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-var bs = require('bootstrap');
-
 // create a new express server
 var app = express();
 
