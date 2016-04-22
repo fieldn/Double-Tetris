35ab61c2bc722e2157d760062ba03fed2b1f25f3
Noah Field <fieldn@purdue.edu>
2016-04-22 16:03:47 -0400

    arrrgghhhhhh I can't decide

--- public/includes/navbar.js
+++ public/includes/navbar.js
@@ -3,13 +3,16 @@ document.write(
 "		<div class='container-fluid'>" +
 "			<!-- Brand and toggle get grouped for better mobile display -->" +
 "			<div class='navbar-header'>" +
+"			<a class='navbar-brand' href='index.html'>" +
+"		        <img alt='TetrisHeaven' src='images/th3.png'>" + 
+"				</a>" +
 "				<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>" +
 "					<span class='sr-only'>Toggle navigation</span>" +
 "					<span class='icon-bar'></span>" +
 "					<span class='icon-bar'></span>" +
 "					<span class='icon-bar'></span>" +
 "				</button>" +
-"				<a class='navbar-brand' href='index.html'>TetrisTetris</a>" +
+"				<a class='navbar-brand' href='index.html'>TetrisHeaven</a>" +
 "			</div>" +
 "" + 
 "			<!-- Collect the nav links, forms, and other content for toggling -->" +
