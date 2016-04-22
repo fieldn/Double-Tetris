3385240b13167369d134c2f7331110c248caf2b3
Noah Field <fieldn@purdue.edu>
2016-04-22 01:05:23 -0400

    Added modularity of navbar and basic html for all pages

---
+++ public/includes/navbar.js
@@ -0,0 +1,31 @@
+document.write(
+"	<nav class='navbar navbar-default'>" +
+"		<div class='container-fluid'>" +
+"			<!-- Brand and toggle get grouped for better mobile display -->" +
+"			<div class='navbar-header'>" +
+"				<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>" +
+"					<span class='sr-only'>Toggle navigation</span>" +
+"					<span class='icon-bar'></span>" +
+"					<span class='icon-bar'></span>" +
+"					<span class='icon-bar'></span>" +
+"				</button>" +
+"				<a class='navbar-brand' href='index.html'>TetrisTetris</a>" +
+"			</div>" +
+"" + 
+"			<!-- Collect the nav links, forms, and other content for toggling -->" +
+"			<div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>" +
+"				<ul class='nav navbar-nav'>" +
+"					<li><a href='recovery.html'>Recovery</a></li>" +
+"					<li><a href='versus.html'>Versus</a></li>" +
+"					<li><a href='enemy.html'>Your Own Worst Enemy</a></li>" +
+"					<li><a href='classic.html'>Classic</a></li>" +
+"					<li><a href='leaderboard.html'>Leaderboard</a></li>" +
+"				</ul>" +
+"" +
+"				<ul class='nav navbar-nav navbar-right'>" +
+"					<li><a href='#'>Login</a></li>" +
+"				</ul>" +
+"			</div><!-- /.navbar-collapse -->" +
+"		</div><!-- /.container-fluid -->" +
+"	</nav>"
+);
